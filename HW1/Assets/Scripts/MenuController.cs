230a8e7d04c54ebca4d7621452bb6a3e5ab3d5ef
Unknown <guilhem.remburre@epitech.eu>
2018-02-07 13:20:55 -0800

    Menu OK

--- HW1/Assets/Scripts/MenuController.cs
+++ HW1/Assets/Scripts/MenuController.cs
@@ -5,21 +5,13 @@ using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour {
 
-    private int buttonWidth;
-    private int buttonHeight;
-    
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     public void changeScene(string sceneName)
     {
         Application.LoadLevel(sceneName);
+    }
+
+    public void quitApplication()
+    {
+        Application.Quit();
     }
 }
