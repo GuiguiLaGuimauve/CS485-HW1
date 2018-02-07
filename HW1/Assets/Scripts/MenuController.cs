4eb6d6f2364cdee7fb77a809aec9dbf1e606dc31
Unknown <guilhem.remburre@epitech.eu>
2018-02-07 09:29:54 -0800

    Add menu in Scene 3
    Menu with two buttons to go on Scene 1 or Scene 2

---
+++ HW1/Assets/Scripts/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+    private int buttonWidth;
+    private int buttonHeight;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void changeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
